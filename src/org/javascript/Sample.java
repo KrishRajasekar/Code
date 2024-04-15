eb635b8039bd2b23430cccf147085a8ea2249244
KrishRajasekar <rajasekar93446@gmail.com>
2024-04-15 18:20:25 +0530

    Selenium

--- src/org/javascript/Sample.java
+++ src/org/javascript/Sample.java
@@ -9,11 +9,19 @@ import org.openqa.selenium.support.ui.Select;
 
 public class Sample {
 	
+	private void EmpName() {
+		System.out.println("Empname : Rajasekar");
+	}
+	private void EmpID() {
+		System.out.println("EmpId : 346766");
+	}
+	private void EmpClgname() {
+		System.out.println("Empclgname : K.S.R college of arts and science");
+	}
+	private void EmpAdd() {
+		System.out.println("EmpCity : Chennai");
+	}
 	public static void main(String[] args) {
-		
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.facebook.com/");
-		
 	
 		
 	}
